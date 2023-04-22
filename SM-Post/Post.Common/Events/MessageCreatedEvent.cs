f112d6aa01ee915c8ae7268ed7dafb016dc3fc59
Maryia Paliashchuk <maryia.paliashchuk@gmail.com>
2023-04-22 14:29:43 +0300

    Events added

---
+++ SM-Post/Post.Common/Events/MessageCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    public class MessageCreatedEvent : BaseEvent
+    {
+        public MessageCreatedEvent() : base(nameof(MessageCreatedEvent))
+        {
+        }
+
+        public string Message { get; set; }
+    }
+}
